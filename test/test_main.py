8871e4be53f41b5bc60395cc971e2c72507b2843
Ingrid-Nataly-Cruz-Espinosa <1719110501@utectulancingo.edu.mx>
2022-06-22 08:02:44 +0000

    Por fin :)

--- test/test_main.py
+++ test/test_main.py
@@ -6,32 +6,40 @@ def test_index():
     response = clientes.get("/") # requests
     data = {"message":"Hello World"}
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json()      == data
 
 def test_clientes():
     response = clientes.get("/clientes/") #requests
     data = [{"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"},
     {"id_cliente":2,"nombre":"John","email":"john@email.com"},
-    {"id_cliente":3,"nombre":"Carthoris","email":"carthotis@email.com"}]
+    {"id_cliente":3,"nombre":"Carthoris","email":"carthotis@email.com"},
+    {"id_cliente":4,"nombre":"Fer","email":"fer@email.com"}]
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json()      == data
 
 def test_client1():
     response = clientes.get("/clientes/1") #requests
-    data = [{"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}]
+    data     = [{"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}]
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json()      == data
 
 def test_clientpost():
-    payload = {"id_cliente":4,"nombre":"Fer","email":"fer@email.com"}
+    payload  = {"id_cliente":4,"nombre":"Fer","email":"fer@email.com"}
     response = clientes.post("/clientes/", json=payload)
-    data = {"message":"Insertaste un cliente correctamente :)"}
+    data     = {"message":"Insertaste un cliente correctamente :)"}
     assert response.status_code == 200
-    assert response.json() == data
+    assert response.json()      == data
+
+def test_clientdelet():
+    payload  = {"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}
+    response = clientes.delete("/clientes/1", json=payload)
+    data     = {"message":"Eliminaste un cliente correctamente :("}
+    assert response.status_code == 200
+    assert response.json()      == data
 
 def test_clientput():
-    payload = {"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}
+    payload  = {"id_cliente":4,"nombre":"Fer","email":"fer@email.com"}
     response = clientes.put("/clientes/", json=payload)
-    data = {"message":"Actualizaste un cliente correctamente :)"}
+    data     = {"message":"Actualizaste un cliente correctamente :)"}
     assert response.status_code == 200
-    assert response.json() == data    
+    assert response.json()      == data        
