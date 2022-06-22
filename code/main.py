7c602e229aa2c1cc15061a04a4ea23320bb35422
Ingrid-Nataly-Cruz-Espinosa <1719110501@utectulancingo.edu.mx>
2022-06-22 07:27:58 +0000

    ya casii:(

--- code/main.py
+++ code/main.py
@@ -11,6 +11,10 @@ class Cliente(BaseModel):
     nombre: str
     email: str
 
+class regcliente(BaseModel):
+    nombre: str
+    email : str    
+
 app = FastAPI()
 
 
@@ -19,7 +23,7 @@ async def index():
     return{"message":"Hello World"}
 @app.get("/clientes/", response_model=List[Cliente])
 async def clientes():
-    with sqlite3.connect('sql/clientes.sqlite') as connection:
+    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM clientes")
@@ -28,12 +32,44 @@ async def clientes():
 
 @app.get("/clientes/{id_cliente}")
 async def clientes(id_cliente):
-    with sqlite3.connect('sql/clientes.sqlite') as connection:
+    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor=connection.cursor()
         cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id_cliente)))
         response=cursor.fetchall()
-        return response        
+        return response
+
+@app.post("/clientes/", response_model=Respuesta)
+def post_cliente(cliente: regcliente):
+    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor=connection.cursor()
+        cursor.execute("INSERT INTO clientes(nombre,email) VALUES(?,?)", (cliente.nombre,cliente.email))
+        cursor.fetchall()
+        response = {"message":"Insertaste un cliente correctamente :)"}
+        return response
+
+
+@app.put("/clientes/", response_model=Respuesta)
+async def clientes_update(nombre: str="", email:str="", id_cliente:int=0):
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("UPDATE clientes SET nombre =?, email= ? WHERE id_cliente =?;",(nombre, email, id_cliente))
+        connection.commit()
+        response = {"message":"Actualizaste un cliente correctamente :)"}
+        return response
+
+
+@app.delete("/clientes/{id}")
+async def clientes_delete(id):
+    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
+        connection.row_factory = sqlite3.Row
+        cursor=connection.cursor()
+        cursor.execute("DELETE FROM clientes WHERE id_cliente ={}".format(int(id)))
+        cursor.fetchall()
+        response = {"message":"Eliminaste un cliente correctamente :("}
+        return response               
 
 
 
--- test/test_main.py
+++ test/test_main.py
@@ -7,3 +7,31 @@ def test_index():
     data = {"message":"Hello World"}
     assert response.status_code == 200
     assert response.json() == data
+
+def test_clientes():
+    response = clientes.get("/clientes/") #requests
+    data = [{"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"},
+    {"id_cliente":2,"nombre":"John","email":"john@email.com"},
+    {"id_cliente":3,"nombre":"Carthoris","email":"carthotis@email.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_client1():
+    response = clientes.get("/clientes/1") #requests
+    data = [{"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_clientpost():
+    payload = {"id_cliente":4,"nombre":"Fer","email":"fer@email.com"}
+    response = clientes.post("/clientes/", json=payload)
+    data = {"message":"Insertaste un cliente correctamente :)"}
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_clientput():
+    payload = {"id_cliente":1,"nombre":"Dejah","email":"deja@email.com"}
+    response = clientes.put("/clientes/", json=payload)
+    data = {"message":"Actualizaste un cliente correctamente :)"}
+    assert response.status_code == 200
+    assert response.json() == data    
